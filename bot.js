9343b928a1e5fbdbefb90312e8fccbb302dad14d
Steven Xu <s@stevenxu.xyz>
2017-06-06 15:16:11 +0000

    use promises for listing users, add comments

--- bot.js
+++ bot.js
@@ -4,14 +4,15 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var includes = require('underscore.string/include');
 var startsWith = require('underscore.string/startsWith');
+var promisifyAll = require("bluebird").promisifyAll;
 
 var bot_token = process.env.SLACK_BOT_TOKEN;
 if (!bot_token) throw new Error("Need to provide SLACK_BOT_TOKEN environment variable");
 var token = process.env.SLACK_API_TOKEN;
 if (!token) throw new Error("Need to provide SLACK_API_TOKEN environment variable");
 
-var web = new WebClient(token);
-var rtm = new RtmClient(bot_token);
+var web = promisifyAll(new WebClient(token));
+var rtm = promisifyAll(new RtmClient(bot_token));
 
 var botUser;
 var list; 
@@ -30,37 +31,48 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     
     if (startsWith(messageText, 'add user')) {
       let usersIdToAdd = messageText.replace(/add user(s)? /, '').split(/s+/);
-      let allUsers = web.users.list(false, (err, res) => {
-        if (err || !res.ok) {
-         // TODO add error message
-        } else {
-          let userMap = {};
-          res.results.forEach(user => {userMap[user.name] = user;});
-          
-          let usersToAdd = usersIdToAdd.map(text => {
-            const result = { user: userMap[text], text };
-            return result;
-          });
-          
-          let unknownUsers = [];
-          usersToAdd.forEach(result => {
-            let id;
-            if (result.user) {
-              id = result.user.id; // FIXME
-            } else {
-              id = result.text;
-              unknownUsers.push(result.text);
-            }
-            list[message.channel] = list[message.channel] || [];  // FIXME
-            list[message.channel].push(id);
-          });
-          
-          if (unknownUsers.length) {
-            
+      web.users.listAsync(false)
+      .then((response) => {
+        if (!response.ok) {
+         throw new Error(response);
+        } 
+        
+        // create lookup map for all users by their id
+        let userMap = {};
+        response.results.forEach(user => {userMap[user.name] = user;});
+
+        // look up each user id that the messager asked to add
+        // will be undefined if that user id doesn't exist
+        let usersToAdd = usersIdToAdd.map(text => {
+          const result = { user: userMap[text], text };
+          return result;
+        });
+        
+        // add users to our list. keep track of unknown users
+        let unknownUsers = [];
+        usersToAdd.forEach(result => {
+          let id;
+          if (result.user) {
+            id = result.user.name;
           } else {
-            // TODO "I added `length` users!
+            id = result.text;
+            unknownUsers.push(result.text);
           }
+          list[message.channel] = list[message.channel] || [];  // FIXME
+          list[message.channel].push(id);
+        });
+
+        if (unknownUsers.length) {
+          // TODO "@requester I added `valid users`, but I'm not sure who `unknownUsers` are. Do you want to add them anyways?
+
+        } else {
+          // TODO "I added `length` users!
         }
+        
+      })
+      .catch((err) => {
+        // TODO send message "Something went wrong when trying to add users. try later?"
+        console.error(err);
       });
     } else if (startsWith(messageText, 'remove user')) {
       let userIds = messageText.replace(/add user(s)? /, '').split(/s+/);
