132a4eb0facf91c46cdffc8907157872c65211b5
Steven Xu <s@stevenxu.xyz>
2017-06-13 12:27:55 +0000

    add IntentInterpreter with add/remove tests

---
+++ core/IntentInterpreter.js
@@ -0,0 +1,32 @@
+const startsWith = require('underscore.string/startsWith');
+const utils = require('./utils.js');
+
+module.exports = function (message) {
+  let intent;
+  let entities;
+  
+  if (startsWith(message, utils.BOT_NAME)) {
+    message = message.replace(new RegExp(utils.BOT_NAME + "\\s+"), '');
+  } else {
+    return { isForBot: false };
+  }
+  
+  if (startsWith(message, "add")) {
+    intent = "addUserCommand";
+    const users = message.replace(/add\s+((user(s)?|people)\s+)?/, '').split(/\s+/);
+    entities = { users };
+  } else if (startsWith(message, "remove")) {
+    intent = "removeUserCommand";
+    const users = message.replace(/remove\s+((user(s)?|people)\s+)?/, '').split(/\s+/);
+    entities = { users };
+  } else if (startsWith(message, "list")) {
+    intent = "listUsersCommand";
+    entities = {};
+  } else if (startsWith(message, "pair")) {
+    intent = "pairUsersCommand";
+    entities = {};
+  }
+  
+  // returns {intent, entities}
+  return {intent, entities, isForBot: true};
+};
---
+++ test/IntentIntepreterTest.js
@@ -0,0 +1,56 @@
+/* jshint expr:true */
+/* allow expr because of chai.expect */
+var intentIntepreter = require('../core/IntentInterpreter.js');
+var utils = require('../core/utils.js');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('intent intepreters', function () {
+  it('must translate messages intending to add users', function () {
+    var messages = [
+      utils.BOT_NAME + " add users @joe @amy",
+      utils.BOT_NAME + " add user @joe @amy",
+      utils.BOT_NAME + " add people @joe @amy",
+      utils.BOT_NAME + " add @joe @amy",
+    ];
+    
+    var results = messages.map(intentIntepreter);
+    var expected = [
+      { intent: "addUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "addUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "addUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "addUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true }
+    ];
+     
+    // use indices so we can see which expect failed by looking at
+    // the line number
+    expect(results[0]).to.be.deep.equal(expected[0]);
+    expect(results[1]).to.be.deep.equal(expected[1]);
+    expect(results[2]).to.be.deep.equal(expected[2]);
+    expect(results[3]).to.be.deep.equal(expected[3]);
+  });
+  
+  it('must translate messages intending to remove users', function () {
+    var messages = [
+      utils.BOT_NAME + " remove users @joe @amy",
+      utils.BOT_NAME + " remove user @joe @amy",
+      utils.BOT_NAME + " remove people @joe @amy",
+      utils.BOT_NAME + " remove @joe @amy",
+    ];
+    
+    var results = messages.map(intentIntepreter);
+    var expected = [
+      { intent: "removeUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "removeUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "removeUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true },
+      { intent: "removeUserCommand", entities: {users: ["@joe", "@amy"]}, isForBot: true }
+    ];
+     
+    // use indices so we can see which expect failed by looking at
+    // the line number
+    expect(results[0]).to.be.deep.equal(expected[0]);
+    expect(results[1]).to.be.deep.equal(expected[1]);
+    expect(results[2]).to.be.deep.equal(expected[2]);
+    expect(results[3]).to.be.deep.equal(expected[3]);
+  });
+});
