7132db708ff26e0f7eafdbbf0a82b86b5e951647
Steven Xu <s@stevenxu.xyz>
2017-06-13 12:33:58 +0000

    add list and pair users test for IntentInterpreter

--- test/IntentIntepreterTest.js
+++ test/IntentIntepreterTest.js
@@ -53,4 +53,43 @@ describe('intent intepreters', function () {
     expect(results[2]).to.be.deep.equal(expected[2]);
     expect(results[3]).to.be.deep.equal(expected[3]);
   });
+  
+  it('must translate messages intending to list users', function () {
+    var messages = [
+      utils.BOT_NAME + " list users",
+      utils.BOT_NAME + " list"
+    ];
+    
+    var results = messages.map(intentIntepreter);
+    var expected = [
+      { intent: "listUsersCommand", entities: {}, isForBot: true },
+      { intent: "listUsersCommand", entities: {}, isForBot: true }
+    ];
+     
+    // use indices so we can see which expect failed by looking at
+    // the line number
+    expect(results[0]).to.be.deep.equal(expected[0]);
+    expect(results[1]).to.be.deep.equal(expected[1]);
+  });
+  
+  it('must translate messages intending to pair users', function () {
+    var messages = [
+      utils.BOT_NAME + " pair users",
+      utils.BOT_NAME + " pair",
+      utils.BOT_NAME + " pairs"
+    ];
+    
+    var results = messages.map(intentIntepreter);
+    var expected = [
+      { intent: "pairUsersCommand", entities: {}, isForBot: true },
+      { intent: "pairUsersCommand", entities: {}, isForBot: true },
+      { intent: "pairUsersCommand", entities: {}, isForBot: true }
+    ];
+     
+    // use indices so we can see which expect failed by looking at
+    // the line number
+    expect(results[0]).to.be.deep.equal(expected[0]);
+    expect(results[1]).to.be.deep.equal(expected[1]);
+    expect(results[2]).to.be.deep.equal(expected[2]);
+  });
 });
